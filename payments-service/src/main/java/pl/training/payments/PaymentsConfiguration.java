8736cf5cef70e1e7894e1c3a894c40a2858572d1
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-23 16:49:12 +0200

    base

--- payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
+++ payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
@@ -1,6 +1,5 @@
 package pl.training.payments;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,7 +36,6 @@ public class PaymentsConfiguration {
         return new SystemTimeProvider();
     }
 
-    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
