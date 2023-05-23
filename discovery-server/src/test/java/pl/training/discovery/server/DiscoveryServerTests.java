b8baba47cd0d30fa3f91525aeebc8092e8a39590
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-23 08:30:05 +0200

    base

---
+++ discovery-server/src/test/java/pl/training/discovery/server/DiscoveryServerTests.java
@@ -0,0 +1,13 @@
+package pl.training.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
