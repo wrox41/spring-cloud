91f6e01590c07d0399e33af3e5023f2a8d0d716a
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-24 14:46:40 +0200

    base

--- reactive-payments-service/src/main/resources/static/ratings.js
+++ reactive-payments-service/src/main/resources/static/ratings.js
@@ -1,5 +1,7 @@
 const socket = new WebSocket("ws://localhost:10003/ratings");
 const ratingSpan = document.querySelector("#rating-value");
+const messageInput = document.querySelector("#message");
+const sendBtn = document.querySelector("#send-btn");
 
 socket.onopen = () => console.log("Connected");
 
@@ -9,4 +11,8 @@ socket.onerror = (error) => console.log("Error: " + error);
 
 socket.onmessage = (message) => ratingSpan.innerText = message.data;
 
+sendBtn.addEventListener('click', () => {
+    const message = messageInput.value;
+    socket.send(message);
+});
 
