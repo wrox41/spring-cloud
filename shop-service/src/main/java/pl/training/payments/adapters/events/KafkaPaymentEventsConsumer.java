865187d36876bdeca7216378dce1e7fa0d9e5696
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-26 16:04:06 +0200

    base

--- shop-service/src/main/java/pl/training/payments/adapters/events/KafkaPaymentEventsConsumer.java
+++ shop-service/src/main/java/pl/training/payments/adapters/events/KafkaPaymentEventsConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.function.Consumer;
 
 @Log
-@Component
+@Component("paymentEventsConsumer")
 public class KafkaPaymentEventsConsumer implements Consumer<PaymentEventDto> {
 
     @Override
